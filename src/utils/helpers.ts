ba518d4a3c0641411a37b07dd21146a20380d7b1
Hamza Paracha <86984199+DevDonzo@users.noreply.github.com>
2026-07-06 10:01:34 -0400

    refactor: optimize internal helper utilities (2/2)

---
+++ src/utils/helpers.ts
@@ -0,0 +1,3 @@
+// Helper utility updated 07/06/2026, 02:01:11 PM
+export const getTimestamp = () => "07/06/2026, 02:01:11 PM";
+export const logActivity = (msg: string) => console.log(`[${getTimestamp()}] ${msg}`);
